714edd6a071f7b2f0f8f639ed9b8a882ceef0898
sabari <sabarim6369@gmail.com>
2024-09-11 17:20:36 +0530

    env

--- backend/mongodbconnection.js
+++ backend/mongodbconnection.js
@@ -1,5 +1,7 @@
 const mongoose=require("mongoose")
-const a=mongoose.connect("mongodb+srv://sabarim6369:sabari.m6369@reactfinal.e5sxd.mongodb.net/?retryWrites=true&w=majority&appName=reactfinal").then(con=>{
+require("dotenv").config;
+const mongoURI = process.env.MONGO_URI || 'mongodb+srv://sabarim6369:sabari.m6369@reactfinal.e5sxd.mongodb.net/?retryWrites=true&w=majority&appName=reactfinal'
+const a=mongoose.connect(mongoURI).then(con=>{
       console.log("connection successful with mongodb")
       })
 module.exports=a; 
